e6dc021500c27fd6df7159edc089dc327984b8b1
Michael MacDonald <mikemacd@gmail.com>
2020-12-01 01:20:49 -0500

    Day one, part b completed

--- Day01/Day01b/day01b.go
+++ Day01/Day01b/day01b.go
@@ -4,42 +4,68 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strconv"
-	"strings"
+	//	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
+	numbers := readInput()
+
+	v := threeSum(numbers, 2020)
+	if len(v) > 0 {
+		fmt.Printf("%d *%d * %d == %d\n", numbers[v[0]], numbers[v[1]], numbers[v[2]], numbers[v[0]]*numbers[v[1]]*numbers[v[2]])
+		os.Exit(0)
+	}
+	fmt.Printf("Nothing found\n")
+	os.Exit(0)
+
+}
+
+func threeSum(numbers []int, target int) []int {
+	m := make(map[int]int)
+	for idx, num := range numbers {
+		if n := twoSum(numbers, target-num); len(n) > 0 {
+			return []int{n[0], n[1], idx}
+		}
+		m[num] = idx
+	}
+	return []int{}
+}
+
+func twoSum(numbers []int, target int) []int {
+	m := make(map[int]int)
+	for idx, num := range numbers {
+		if v, found := m[target-num]; found {
+			return []int{v, idx}
+		}
+		m[num] = idx
+	}
+	return []int{}
+}
+
+func readInput() []int {
+	var numbers []int
+
 	if len(os.Args) < 2 {
 		fmt.Println("Missing parameter, provide file name!")
-		return
+		return []int{}
 	}
-
 	data, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		fmt.Println("Can't read file:", os.Args[1])
 		panic(err)
 	}
 
-	sum := 0
-	for i, line := range bytes.Split(data, []byte("\n")) {
-		number := strings.TrimSpace(string(line))
-		if number != "" {
-			n, err := strconv.Atoi(string(number))
-			if err != nil {
-				fmt.Printf("Could not convert line %d (%s) to int\n", i, number)
-			}
-			sum += fuelForFuel(n)
+	lines := bytes.Split(data, []byte("\n"))
+	for i, line := range lines {
+		num, err := strconv.Atoi(string(line))
+		if err != nil {
+			log.Fatalf("Can't parse number on line %d: %v\n", i, line)
 		}
+		numbers = append(numbers, num)
 	}
-	fmt.Printf("Sum: %d\n", sum)
 
-}
-
-func fuelForFuel(f int) int {
-	f4f := (f / 3) - 2
-	if f4f > 0 {
-		return f4f + fuelForFuel(f4f)
-	}
-	return 0
+	return numbers
 }
